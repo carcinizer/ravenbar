408bb7ebed0b391c9a28c539b1b37a8d08608bc5
Michał Moniewski <mmoniewski@protonmail.com>
2020-08-15 02:54:51 +0200

    Drawing pt 2

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 
 use x11rb::connection::Connection;
 use x11rb::protocol::Event;
+use x11rb::protocol::xproto::Rectangle;
 
 mod window;
 
@@ -9,12 +10,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let (conn, screen_num) = x11rb::connect(None).unwrap();
     let screen = &conn.setup().roots[screen_num];
 
-    let wnd = window::Window::new(&conn, &screen);
+    let wnd = window::Window::new(&conn, &screen)?;
 
     loop {
         let event = conn.wait_for_event()?;
         println!("{:?}", event);
         
+        window::Drawable::Rect(window::Color::new(255,255,0,255)).draw(&wnd, Rectangle{x:10, y:10, width:100, height:100})?;
+
         match event {
             Event::KeyPress(key) => {eprintln!("{:?}", key);},
             _ => {}
--- src/window.rs
+++ src/window.rs
@@ -4,15 +4,30 @@ use std::error::Error;
 use x11rb::connection::Connection;
 use x11rb::protocol::xproto::*;
 use x11rb::protocol::Event;
+use x11rb::errors::ConnectionError;
 
 
 pub struct Window<'a, T: Connection> {
     window : u32,
-    gc : u32,
+    colormap : u32,
     conn : &'a T
 }
 
-pub struct Color(u8,u8,u8,u8);
+pub struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8
+}
+
+impl Color {
+    pub fn new(r: u8, g: u8, b: u8, a: u8) -> Color {
+        Color{r,g,b,a}
+    }
+    pub fn as_xcolor(&self) -> u32 {
+        ((self.a as u32) << 24) | ((self.r as u32) << 16) | ((self.g as u32) << 8) | (self.b as u32)
+    }
+}
 
 pub enum Drawable<'a> {
     Rect(Color),
@@ -20,34 +35,47 @@ pub enum Drawable<'a> {
 }
 
 impl Drawable<'_> {
-    pub fn draw<T: Connection>(&self, window: Window<T>, rect: Rectangle) {
+    pub fn draw<T: Connection>(&self, window: &Window<T>, rect: Rectangle) -> Result<(), Box<dyn Error>> {
         match self {
             Drawable::Rect(c) => {
-                window.conn.poly_fill_rectangle(window.window, window.gc, &[rect]);
+                let gc = window.conn.generate_id()?;
+
+                window.conn.create_gc(gc, window.window, &CreateGCAux::new().foreground(c.as_xcolor()))?;
+                window.conn.poly_fill_rectangle(window.window, gc, &[rect])?;
+                
+                window.conn.flush()?;
+
+                window.conn.free_gc(gc)?;
             }
 
             Drawable::Text(c, text) => {
                 // TODO
             }
         }
+        Ok(())
     }
 }
 
 impl<T: Connection> Window<'_, T> {
     pub fn new<'a>(conn: &'a T, screen: &Screen) -> Result<Window<'a, T>, Box<dyn Error>> {
                 
-        let window  = conn.generate_id()?;
-        let gc      = conn.generate_id()?;
+        let window   = conn.generate_id()?;
 
         conn.create_window(x11rb::COPY_DEPTH_FROM_PARENT, window, screen.root,
                            0,0,300,300, 0, WindowClass::InputOutput, 0,
-                           &CreateWindowAux::new().background_pixel(screen.white_pixel))?;
+                           &CreateWindowAux::new()
+                                .background_pixel(Color::new(255,100,200,150).as_xcolor())
+                                .event_mask(EventMask::Exposure
+                                          | EventMask::ButtonPress
+                                          | EventMask::ButtonRelease
+                                          | EventMask::PointerMotion))?;
 
-        conn.create_gc(gc, window, &CreateGCAux::new().foreground(screen.black_pixel))?;
+
+        let colormap = screen.default_colormap;
 
         conn.map_window(window)?;
         conn.flush()?;
 
-        Ok( Window {window, gc, conn} )
+        Ok( Window {window, colormap, conn} )
     }
 }
