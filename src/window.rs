f92309d2f6bb451231f46837efa21b349f0a40cd
Michał Moniewski <mmoniewski@protonmail.com>
2020-09-04 18:43:02 +0200

    No XRender needed

--- src/window.rs
+++ src/window.rs
@@ -4,8 +4,6 @@ use std::collections::HashMap;
 use std::rc::Rc;
 
 use x11rb::protocol::xproto::*;
-use x11rb::protocol::render;
-use x11rb::protocol::render::ConnectionExt as _;
 use x11rb::protocol::Event;
 use x11rb::errors::ConnectionError;
 use x11rb::connection::Connection;
