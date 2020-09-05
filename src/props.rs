7c2e0ea8340890918a6a2e69a8d4f55515904574
Michał Moniewski <mmoniewski@protonmail.com>
2020-09-05 14:50:51 +0200

    Move config props from config.rs to props.rs

--- src/props.rs
+++ src/props.rs
@@ -2,6 +2,8 @@
 
 use std::collections::HashMap;
 
+use serde::Deserialize;
+
 use crate::event::Event;
 use crate::command::*;
 use crate::window::{Drawable, Direction};
@@ -77,6 +79,23 @@ pub struct BarPropsCurrent {
     pub height: u16
 }
 
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct BarConfigProps {
+    pub alignment: Option<String>,
+    pub height: Option<u16>
+}
+
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct BarConfigWidgetProps {
+    pub background: Option<String>,
+    pub foreground: Option<String>,
+    pub command: Option<String>,
+    pub border_factor: Option<f32>,
+    pub interval: Option<f32>
+}
+
 impl WidgetProps {
     pub fn as_current(&self, e: &Vec<Event>, m: bool) -> WidgetPropsCurrent {
         WidgetPropsCurrent {
@@ -94,5 +113,54 @@ impl BarProps {
             alignment: self.alignment.get(e,m).clone(),
             height: self.height.get(e,m).clone()
         }
+    }
+}
+
+impl BarConfigWidgetProps {
+    pub fn new() -> Self {
+        Self { 
+            background: None,
+            foreground: None,
+            command: None,
+            border_factor: None,
+            interval: None
+        }
+    }
+
+    pub fn mix(&mut self, parent: &Self) -> &mut Self {
+        self.background = mix_options(&parent.background, &self.background);
+        self.foreground = mix_options(&parent.foreground, &self.foreground);
+        self.command    = mix_options(&parent.command,    &self.command);
+        self
+    }
+}
+
+impl BarConfigProps {
+    pub fn new() -> Self {
+        Self {
+            alignment: None,
+            height: None
+        }
+    }
+
+    pub fn mix(&mut self, parent: &Self) -> &mut Self {
+        self.alignment = mix_options(&parent.alignment, &self.alignment);
+        self.height    = mix_options(&parent.height,    &self.height);
+        self
+    }
+}
+
+impl std::default::Default for BarConfigProps {
+    fn default() -> Self { Self::new() }
+}
+
+impl std::default::Default for BarConfigWidgetProps {
+    fn default() -> Self { Self::new() }
+}
+
+fn mix_options<T: Clone>(parent: &Option<T>, child: &Option<T>) -> Option<T> {
+    match child {
+        Some(x) => Some(x.clone()),
+        None => match parent {Some(y) => Some(y.clone()), None => None}
     }
 }
